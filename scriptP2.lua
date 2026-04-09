ad90577e93595b4923779af9dc4cdafd5d6f7955
rafaelsantosgumball-commits <rafaelsantos.gumball@gmail.com>
2026-04-09 00:56:16 -0300

    Disable external script loading in V function
    Comment out loadstring function to prevent execution of external script.

--- scriptP2.lua
+++ scriptP2.lua
@@ -172,11 +172,11 @@ end
 
 local function V()
 	return 0
-    loadstring(
-        game:HttpGet(
-            "https://api.luarmor.net/files/v3/loaders/" .. tostring(G) .. ".lua"
-        )
-    )()
+    //loadstring(
+    //    game:HttpGet(
+        //    "https://api.luarmor.net/files/v3/loaders/" .. tostring(G) .. ".lua"
+      //  )
+    //)()
 end
 
 
